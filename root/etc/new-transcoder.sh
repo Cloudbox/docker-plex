54bae35ee03756e7c31a8336a7344407755dad8c
desimaniac <5501908+desimaniac@users.noreply.github.com>
2019-08-01 09:16:41 -0500

    Transcoder: Corrected bash testing.

--- root/etc/new-transcoder.sh
+++ root/etc/new-transcoder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 marap=$(cut -c 10-14 <<<"$@")
-if [ $marap <> "mpeg4" ]; then
-     exec /usr/lib/plexmediaserver/Plex\ Transcoder2 -hwaccel nvdec "$@"
-else
+if [[ $marap == "mpeg4" ]]; then
      exec /usr/lib/plexmediaserver/Plex\ Transcoder2 "$@"
+else
+     exec /usr/lib/plexmediaserver/Plex\ Transcoder2 -hwaccel nvdec "$@"
 fi
