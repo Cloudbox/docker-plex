8a8abd528c894db3ec87ee098c05be0770460d1b
desimaniac <5501908+desimaniac@users.noreply.github.com>
2019-08-02 12:31:02 -0500

    Transcoder: Codec lookup is now flexible to various argument positions.

--- root/etc/new-transcoder.sh
+++ root/etc/new-transcoder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-marap=$(cut -c 10-14 <<<"$@")
+marap=$(grep -oP '(?<=-codec:0 )[^ ]*' <<< "$@" | head -1)
 if [[ $marap == "mpeg4" ]]; then
      exec /usr/lib/plexmediaserver/Plex\ Transcoder2 "$@"
 else
