9477e912cc984995858c5bfcba2370cc561c88f4
l3uddz <l3uddz@gmail.com>
2020-01-26 09:58:41 +0000

    change(cleanup): relay disabling and nvdec is now part of parent image (#7)

--- root/etc/new-transcoder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-marap=$(grep -oP '(?<=-codec:0 )[^ ]*' <<< "$@" | head -1)
-if [[ $marap == "mpeg4" ]]; then
-     exec /usr/lib/plexmediaserver/Plex\ Transcoder2 "$@"
-else
-     exec /usr/lib/plexmediaserver/Plex\ Transcoder2 -hwaccel nvdec "$@"
-fi
